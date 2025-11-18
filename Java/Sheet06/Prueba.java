5cc99b74017f2375599e10c976fa76ba05cf3041
Ignacio Pérez <20.ignaciope.2c@gmail.com>
2025-11-18 18:58:08 +0100

    Sheet06 Ejercicio2

--- Java/Sheet06/Prueba.java
+++ Java/Sheet06/Prueba.java
@@ -5,12 +5,24 @@ public class Prueba {
 
     public static void main(String[] args) {
         
+        //Ejercicio 01 --------------------------------------------------
+
         List<String> nombres = List.of("Ana Ruiz", "Juan Pérez", "María Torres", "Sol", "Pedro López");
-        List<String> resultado = nombres.stream()
+        List<String> res = nombres.stream()
             .filter(nombre -> nombre.length() > 9)
             .map(String::toUpperCase)
             .collect(Collectors.toList());
 
-        System.out.println(resultado);
+        System.out.println(res);
+
+        //Ejercicio 02 ------------------------------------------------
+
+        List<Integer> numeros = List.of(5, 12, 47, 105, 89, 2, 100, 34);
+        int resu = numeros.stream()
+            .filter(numero -> numero >10 && numero<100)
+            .mapToInt(Integer::intValue)
+            .sum();
+
+        System.out.println("Suma = " + resu);
     }
 }
